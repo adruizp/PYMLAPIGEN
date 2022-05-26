05d5f3e941798ab8d892fa979c26997db33c047a
Adrián Ruiz Parra <adruizp@alumnos.unex.es>
2022-05-26 19:11:01 +0200

    Mejora filtros (operador + activo/desactivo)
    Mejora de los filtros:
    - Se han añadido los operadores ("contiene",==,>=,>,<,<=)
    - Se ha añadido una opción para marcar o desmarcar un filtro sin necesidad de ser eliminado
    - Si un filtro no tiene ningun valor en el input, no se aplicará
    - Ejecución del filtro al marcar/desmarcar un filtro, al eliminar un filtro y al elegir el campo o el operador

--- pymlapigen/static/js/filterTable.js
+++ pymlapigen/static/js/filterTable.js
@@ -6,57 +6,70 @@ filterDiv.querySelector('.minus').style.display = ''
  * Filtra una tabla HTML
  */
 function filter() {
-  var filter, column, index, table, tr, td, i, j, txtValue, toDisplay;
+  var table, tr, td, i, j, tdValue, toDisplay;
 
   //Obtener los inputs
+  var include = document.querySelectorAll(".filterInclude")
   var columns = document.querySelectorAll(".filterColumn")
+  var ops = document.querySelectorAll(".filterOp")
   var inputs = document.querySelectorAll(".filterInput")
 
-  for (i = 0; i < inputs.length; i++) {
-    console.log(i)
-
-    filter = inputs[i].value.toUpperCase();
-
-    //Obtener la columna
-    column = columns[i];
-    index = column.selectedIndex + 1;
-
-    //Procesar tabla
-    table = document.getElementById("datasetTable");
-    tr = table.getElementsByTagName("tr");
+  //Tabla
+  table = document.getElementById("datasetTable");
+  //Filas
+  tr = table.getElementsByTagName("tr");
 
 
 
+  for (i = 0; i < tr.length; i++) {
+    toDisplay = true;
+    for (j = 0; j < inputs.length; j++) {
 
-    for (i = 0; i < tr.length; i++) {
-      toDisplay = true;
-      for (j = 0; j < inputs.length; j++) {
+      //Solo aplicar filtros con valores escritos
+      if (include[j].checked && inputs[j].value != "") {
+
         td = tr[i].getElementsByTagName("td")[columns[j].selectedIndex + 1];
         if (td && toDisplay) {
-          txtValue = td.textContent || td.innerText;
-          if (txtValue.toUpperCase().indexOf(inputs[j].value.toUpperCase()) < 0) {
-            toDisplay = false;
-            console.log("NO ES CORRECTOP")
+          tdValue = td.textContent || td.innerText;
+
+          switch (ops[j].value) {
+            case "contains":
+              toDisplay = tdValue.toUpperCase().indexOf(inputs[j].value.toUpperCase()) > -1;
+              break
+            case "==":
+              toDisplay = tdValue.trim() === inputs[j].value.trim();
+              break
+            case "<":
+              toDisplay = +tdValue < +inputs[j].value;
+              break
+            case "<=":
+              toDisplay = +tdValue <= +inputs[j].value;
+              break
+            case ">":
+              toDisplay = +tdValue > +inputs[j].value;
+              break
+            case ">=":
+              toDisplay = +tdValue >= +inputs[j].value;
+              break
           }
         }
       }
-      if (toDisplay) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
+    }
+    if (toDisplay) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
     }
   }
-
-
 }
+
+
 
 function addfilter() {
   document.getElementById("aditionalFilters").appendChild(filterDiv.cloneNode(true));
 }
 
 function deletefilter(filterId) {
-  console.log("WORKS")
   filterId.parentElement.parentElement.remove()
-
+  filter()
 }
