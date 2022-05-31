85883e3b19f76358dc3367a25c2af574cb5d8bb3
Adrián Ruiz Parra <adruizp@alumnos.unex.es>
2022-05-31 17:53:56 +0200

    Incluir opcion de seleccionar puerto y mejora visual de modificaciones
    - Ahora es posible elegir la ip y el puerto a lanzar la aplicacion desde el comando de lanzamiento (pymlapigen <ip> <puerto> / python run.py <ip> <puerto>
    
    - Mejoras visuales para las modificaciones. Ahora aparecen alertas y se cambia el color del boton a amarillo al intentar modificar una api generada.

--- pymlapigen/cli.py
+++ pymlapigen/cli.py
@@ -6,8 +6,12 @@ def cli():
     # Loads configuration 
     pymlapigen.flask_app.config['APP_FOLDER'] = os.path.dirname(pymlapigen.__file__)
 
-    # Check if user did input hosting IP
+    # Default ip and port
     if len(sys.argv) == 1:
         pymlapigen.flask_app.run()
-    else:
+    # Custom ip
+    elif len(sys.argv) == 2:
         pymlapigen.flask_app.run(host=sys.argv[1])
+    # Custom ip and port
+    elif len(sys.argv) == 3:
+        pymlapigen.flask_app.run(host=sys.argv[1],port=sys.argv[2])
--- run.py
+++ run.py
@@ -6,6 +6,10 @@ import os
 if (__name__ == "__main__"):
     pymlapigen.flask_app.config['APP_FOLDER'] = os.path.join(os.getcwd(), "pymlapigen")
     if len(sys.argv) == 1:
-        pymlapigen.flask_app.run(debug=True)
-    else:
-        pymlapigen.flask_app.run(host=sys.argv[1])
+        pymlapigen.flask_app.run(debug=True)   
+    # Custom ip
+    elif len(sys.argv) == 2:
+        pymlapigen.flask_app.run(debug=True, host=sys.argv[1])
+    # Custom ip and port
+    elif len(sys.argv) == 3:
+        pymlapigen.flask_app.run(debug=True, host=sys.argv[1],port=sys.argv[2])
